6dcab538cadd7cc75a41be27b19be58c79f54953
allmerlukas <allmerlukas@gmail.com>
2026-06-10 08:50:08 +0200

    chore: remove giveaway system entirely

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,3 @@
-const { initGiveaways }  = require('../utils/giveawayManager');
 const { startAutoWave }  = require('../utils/autoWaveEngine');
 
 module.exports = {
@@ -7,19 +6,14 @@ module.exports = {
   async execute(client) {
     console.log(`\n✅ Wind Bot is online!`);
     console.log(`🤖 Logged in as: ${client.user.tag}`);
-    console.log(`📡 Tracking partner links in all channels`);
-    console.log(`🎁 Checking for active giveaways...\n`);
+    console.log(`📡 Tracking partner links in all channels\n`);
 
     client.user.setPresence({
       activities: [{ name: 'partner links 🔗', type: 3 }], // 3 = Watching
       status: 'online',
     });
 
-    // Resume any giveaways that were running before the bot restarted
-    await initGiveaways(client);
-
     // Start the Auto-Wave engine (first tick after 30 min)
     startAutoWave(client);
   }
 };
-
